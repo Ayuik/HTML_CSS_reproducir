e359fa748d6ec3f5282d398c163159a8056552cf
Ayuik <marcosayelen@gmail.com>
2025-01-14 09:11:13 +0100

    correction finished

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const allBoxes = container.querySelectorAll('.box');
 
+allBoxes.forEach((box) => {
+    box.addEventListener('click', (event) => {
+        box.classList.toggle('hidden')
+    });
+})
